a598572cd727ed1573bc87bc5a014483541eceea
Yassine Maamri <ymvtbusiness@gmail.com>
2023-06-30 01:42:34 +0100

    new update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,19 +31,17 @@ const Header = () => (
     </Div1>
     <Div2>
       <li>
-        <Link href="#projects">
-          <NavLink onClick={() => scrollToSection("projects")}>
-            Projects
-          </NavLink>
+        <Link href="#projects" onClick={() => scrollToSection("projects")}>
+          <NavLink>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#tech">
+        <Link href="#tech" onClick={() => scrollToSection("tech")}>
           <NavLink>Technologies</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#about">
+        <Link href="#about" onClick={() => scrollToSection("about")}>
           <NavLink>About</NavLink>
         </Link>
       </li>
