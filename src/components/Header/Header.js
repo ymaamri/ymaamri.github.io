239bd5cf7d5f8753ab5fe10dcaa6aa0b472edbbb
Yassine Maamri <ymvtbusiness@gmail.com>
2023-06-30 10:21:33 +0100

    new update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,7 @@ import React from "react";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 import { DiCssdeck } from "react-icons/di";
 
-import {
-  Container,
-  Div1,
-  Div2,
-  Div3,
-  NavLink,
-  SocialIcons,
-} from "./HeaderStyles";
+import { Container, Div1, Div2, Div3, Navo, SocialIcons } from "./HeaderStyles";
 
 const scrollToSection = (id) => {
   const element = document.getElementById(id);
